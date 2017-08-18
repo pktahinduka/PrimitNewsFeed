5e846f147d4a5035008b8fd87ac5c6cfa167e1e3
oxenprogrammer <emmy1000okello@gmail.com>
2017-08-18 03:19:58 -0500

    added a retrieve to the rss

--- rss.py
+++ rss.py
@@ -1,11 +1,11 @@
 import json
-import urllib2
+import urllib.request
 import requests
 
 api_url = "http://34.207.10.230:3000/posts"
 
 # API below returns an array of complex JSON objects
-jsonobj = json.load(urllib2.urlopen(api_url))
+jsonobj = json.load(urllib.request.urlopen(api_url))
 
 # index into the array and access a sub-attribute like this:
 print(jsonobj[5])
@@ -13,6 +13,6 @@ print(jsonobj[5])
 data = {'sender': 'Peter', 'receiver': 'Team 2', 'message': 'WE CRACK THIS EVERYDAY'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 r = requests.post(api_url, data=json.dumps(data), headers=headers)
-
-def getPost():
-    pass
+ 
+local_filename, headers = urllib.request.urlretrieve(api_url)
+html = open(local_filename)
