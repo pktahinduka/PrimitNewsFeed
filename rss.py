5852a1c7b4b062e74136ccb8c12e529424d573df
oxenprogrammer <emmy1000okello@gmail.com>
2017-08-18 03:33:43 -0500

    modified rss.py to python 3

--- rss.py
+++ rss.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 import requests
+import urllib.parse
 
 api_url = "http://34.207.10.230:3000/posts"
 
@@ -10,9 +11,12 @@ jsonobj = json.load(urllib.request.urlopen(api_url))
 # index into the array and access a sub-attribute like this:
 print(jsonobj[5])
 
-data = {'sender': 'Peter', 'receiver': 'Team 2', 'message': 'WE CRACK THIS EVERYDAY'}
+values = {'sender': 'Peter', 'receiver': 'Team 2', 'message': 'WE CRACK THIS EVERYDAY'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-r = requests.post(api_url, data=json.dumps(data), headers=headers)
- 
-local_filename, headers = urllib.request.urlretrieve(api_url)
-html = open(local_filename)
+r = requests.post(api_url, data=json.dumps(values), headers=headers)
+
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(api_url, data, headers)
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
