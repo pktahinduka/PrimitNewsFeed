d3321fd8a3b1bcd11082b73a38eb1112916c265f
oxenprogrammer <emmy1000okello@gmail.com>
2017-08-18 02:46:05 -0500

    branching to emmiPrimitNewsFeed

--- rss.py
+++ rss.py
@@ -8,10 +8,11 @@ api_url = "http://34.207.10.230:3000/posts"
 jsonobj = json.load(urllib2.urlopen(api_url))
 
 # index into the array and access a sub-attribute like this:
-print jsonobj[5]
+print(jsonobj[5])
 
 data = {'sender': 'Peter', 'receiver': 'Team 2', 'message': 'WE CRACK THIS EVERYDAY'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 r = requests.post(api_url, data=json.dumps(data), headers=headers)
 
-def getPost()
+def getPost():
+    pass
